41efa7859aed6e57bf98b2e70183d7fe04faafee
Dhaval.Thanki <dhaval.thanki07@gmail.com>
2022-09-15 16:51:50 +0530

    controller added

---
+++ src/main/java/com/dhaval/myapp1/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.dhaval.myapp1;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class HelloWorldController   
+{  
+@RequestMapping("/")  
+public String hello()   
+{  
+return " Hello Meeting Manager Team";  
+}  
+} 
